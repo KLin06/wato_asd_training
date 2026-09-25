6da4d9e6edc6136b5fd2508e9a684e4d36857514
Ian <163675816+xiongian@users.noreply.github.com>
2026-09-25 00:46:47 -0400

    control: first iteration, crashed unfortunately.

--- src/robot/control/include/control_core.hpp
+++ src/robot/control/include/control_core.hpp
@@ -1,6 +1,14 @@
 #ifndef CONTROL_CORE_HPP_
 #define CONTROL_CORE_HPP_
 
+#include <optional>
+
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/point.hpp"
+#include "geometry_msgs/msg/quaternion.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "nav_msgs/msg/path.hpp"
 #include "rclcpp/rclcpp.hpp"
 
 namespace robot
@@ -8,11 +16,30 @@ namespace robot
 
 class ControlCore {
   public:
-    // Constructor, we pass in the node's RCLCPP logger to enable logging to terminal
-    ControlCore(const rclcpp::Logger& logger);
+    ControlCore(rclcpp::Node& node);
   
   private:
+    void controlLoop();
+    std::optional<geometry_msgs::msg::PoseStamped> findLookaheadPoint();
+    geometry_msgs::msg::Twist computeVelocity(
+      const geometry_msgs::msg::PoseStamped& target);
+    double computeDistance(
+      const geometry_msgs::msg::Point& a,
+      const geometry_msgs::msg::Point& b);
+    double extractYaw(const geometry_msgs::msg::Quaternion& quat);
+
     rclcpp::Logger logger_;
+    rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr path_sub_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
+    rclcpp::TimerBase::SharedPtr control_timer_;
+
+    nav_msgs::msg::Path::SharedPtr current_path_;
+    nav_msgs::msg::Odometry::SharedPtr robot_odom_;
+
+    double lookahead_distance_;
+    double goal_tolerance_;
+    double linear_speed_;
 };
 
 } 
